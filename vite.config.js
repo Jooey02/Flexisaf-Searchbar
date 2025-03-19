bbd97baf58d0f03c77f6dd9fffc3929670ff3543
Jooey Otu <140718796+Jooey02@users.noreply.github.com>
2025-03-19 13:20:04 +0100

    Third update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Flexisaf-Searchbar/"
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 })
